8ddeb272489c9c929a5393d1a1d38b227e8c2b14
Darren Bennett <dbennett2911@gmail.com>
2021-08-28 11:58:45 +0000

    updates

--- pages/views.py
+++ pages/views.py
@@ -12,7 +12,12 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     #return HttpResponse("<h1>About Page</h1>") # String of HTML Code
-    return render(request, "about.html", {})
+    my_context = {
+      "my_text": "This is about us",
+      "my_number": 123,
+      "my_list": [123, 3456, 4567]
+    }
+    return render(request, "about.html", my_context)
 
 def blog_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Blog Page</h1>") # String of HTML Code
