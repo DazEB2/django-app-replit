0be4edc4e5c3c560cd517bd95e0b55be0ca942ce
Darren Bennett <dbennett2911@gmail.com>
2021-08-28 10:59:43 +0000

    updates

--- pages/views.py
+++ pages/views.py
@@ -7,13 +7,18 @@ def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>") # String of HTML Code
+    #return HttpResponse("<h1>Contact Page</h1>") # String of HTML Code
+    return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>") # String of HTML Code
+    #return HttpResponse("<h1>About Page</h1>") # String of HTML Code
+    return render(request, "about.html", {})
 
 def blog_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Blog Page</h1>") # String of HTML Code
+    #return HttpResponse("<h1>Blog Page</h1>") # String of HTML Code
+    return render(request, "blog.html", {})
+
 
 def socials_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Socials Page</h1>") # String of HTML Code    
+    #return HttpResponse("<h1>Socials Page</h1>") # String of HTML Code 
+    return render(request, "socials.html", {})   
